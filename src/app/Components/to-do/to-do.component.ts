8002b27e3b4db34aeefd1162f3e442c1a89724d7
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-05-01 21:30:50 +0300

    finishing adding a new task

--- src/app/Components/to-do/to-do.component.ts
+++ src/app/Components/to-do/to-do.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-to-do',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './to-do.component.html',
-  styleUrl: './to-do.component.css'
+  styleUrl: './to-do.component.css',
 })
 export class ToDoComponent {
-title:string="To Do List Application"
-imagePath:string="https://cdn4.iconfinder.com/data/icons/global-logistics-3/512/104-1024.png"
+  title: string = 'To Do List Application';
+  imagePath: string =
+    'https://cdn4.iconfinder.com/data/icons/global-logistics-3/512/104-1024.png';
+
+  tasks: string[] = [];
+  newTask: string = '';
+  isAvailable:boolean=false
+  addTask() {
+    if (this.newTask !== null && this.newTask !== '') {
+      this.tasks.push(this.newTask.trim());
+      this.newTask = '';
+      this.isAvailable=true
+    }
+  }
+  removeTask() {}
+  editTask() {}
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,25 @@ import { MatButtonModule } from '@angular/material/button';
 import { SliderModule } from 'primeng/slider';
 import { FormsModule } from '@angular/forms';
 import { RadioButtonModule } from 'primeng/radiobutton';
+import { ButtonModule } from 'primeng/button';
+import { ToDoComponent } from './Components/to-do/to-do.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatButtonModule, SliderModule,FormsModule,RadioButtonModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatButtonModule,
+    SliderModule,
+    FormsModule,
+    RadioButtonModule,
+    ButtonModule,
+    ToDoComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ToDoList';
-  ingredient='maha'
+  ingredient = 'maha';
 }
